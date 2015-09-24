243df9a2d2691dfd0b059e1a9a1d6c97dd282429
aoisensi <aoisensi@mail.com>
2015-09-24 17:53:57 +0900

    first commit 0.1.0.0

---
+++ app/scripts/background.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var services = {
+  'github.com': function githubCom(pathes) {
+    if (pathes.length < 3) {
+      return null;
+    }
+    var ignores = ['issues', 'pulls', 'explore', 'settings', 'blog', 'account'];
+    if (ignores.indexOf(pathes[1]) !== -1) {
+      return null;
+    }
+    return 'github.com/' + pathes[1] + '/' + pathes[2];
+  }
+};
+
+function parse(urls) {
+  var url = new URL(urls);
+  var service = services[url.hostname];
+  if (service === undefined) {
+    return null;
+  }
+  var pathes = url.pathname.split('/');
+  var name = service(pathes);
+  var http = new XMLHttpRequest();
+  http.open('GET', 'http://godoc.org/' + name, false);
+  http.send();
+  if (http.status !== 200) {
+    return null;
+  }
+  return name;
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (parse(tab.url) !== null) {
+    chrome.pageAction.show(tabId);
+  }
+});
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+  var url = 'http://godoc.org/' + parse(tab.url);
+  chrome.tabs.create({
+    url: url
+  });
+});
+//# sourceMappingURL=background.js.map
